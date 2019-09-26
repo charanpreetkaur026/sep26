8d2fa75cb50305cd9c097779a2f63fe391a1a39e
CHARAN <charanpreetkaur026@gmail.com>
2019-09-26 18:46:44 -0400

    second exception

--- src/main/java/ExceptionHandling.java
+++ src/main/java/ExceptionHandling.java
@@ -6,8 +6,11 @@ public class ExceptionHandling
         int a=100;
         int b=0;
         int c;
+
+        int x[]={1,2,3};
         System.out.println("START");
         try {
+            x[4]=1000;
             c = a / b;
             System.out.println(" C:\t" + c);
         }
